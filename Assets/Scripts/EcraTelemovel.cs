d1f3ad864004010a0f046d139ef6aede06859160
SAPHETY\djsantos <david.santos@saphety.com>
2017-05-13 09:59:49 +0100

    Switching text message display to be a gameobject
    This will allow to move the text around

--- Assets/Scripts/EcraTelemovel.cs
+++ Assets/Scripts/EcraTelemovel.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class EcraTelemovel : MonoBehaviour {
 
     public Message startMessage;
+    private List<GameObject> gameObjectsOnScreen;
+    public Vector3 startMessagePosition;
+    
 	// Use this for initialization
 	void Start () {
+        gameObjectsOnScreen = new List<GameObject>();
         ShowMessage(startMessage);
 	}
 	
@@ -18,8 +22,18 @@ public class EcraTelemovel : MonoBehaviour {
 
     void ShowMessage(Message message)
     {
-        Text myText = GetComponent<Text>();
-        myText.text = message.description;
+        foreach (GameObject goMessage in gameObjectsOnScreen)
+        {
+            goMessage.transform.Translate(new Vector3(5, 0, 0));
+        }
+        GameObject newMessage = new GameObject(Random.Range(1, 99999999).ToString());
+        newMessage.transform.SetParent(this.transform);
+        Text newText = newMessage.AddComponent<Text>();
+        newText.text = message.description;
+
+        //Text myText = GetComponent<Text>();
+        //myText.text = message.description;
+
     }
 
 
